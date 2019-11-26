ebf4f54bc9eb14df96e0e1a19624c47778ad56e1
Atsuta Roman <indifferentrun@gmail.com>
2019-11-26 16:14:53 +0700

    little change and build

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,7 @@
-const defaultSliderClass = "liquidSlider";
-
-/**
- * Оборачивает функцию в такую, что после её выполнения будет запущена подписавшаяся функция
- * @param eventFunction функция, на выполнение которой происходит подписка
- * @param subscriber функция-подписчик
- * @param arguments аргументы функции-события
- */
-function addHandler(eventFunction: Function, subscriber: Function, arguments: typeof Array) {
-
-}
+export const defaultSliderClass = "liquidSlider";
+export const viewFunctionNames = [
+    "viewTest", //
+];
+export const modelFunctionNames = [
+    "modelTest",
+];
