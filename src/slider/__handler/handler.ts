37b7e1559b85bc274f0f9b0e3883b922dc5fd2db
Atsuta Roman <indifferentrun@gmail.com>
2019-11-28 13:07:13 +0700

    add parameters

--- src/slider/__handler/handler.ts
+++ src/slider/__handler/handler.ts
@@ -1,14 +1,51 @@
 import {defaultSliderClass} from "../../common";
 
-export class Handler {
-    private _class = defaultSliderClass + "__handler ";
-    private _value: object;
-    private _withTooltip = true;
+export default class Handler {
+    private _defaultClass = `${defaultSliderClass}__handler `;
+    private _value: string;
+    //private _class: string; - надо бы интерфейс сделать или микс
+
+
+    private _isEnd: boolean;
+    get isEnd() {
+        return this._isEnd;
+    };
+
+    set isEnd(value) {
+        this._isEnd = value;
+    };
+
+    set isStart(value: boolean) {
+        this._isEnd = value;
+    }
+
+
+    private _pairedHandler?: Handler;
+
+    /**
+     * Связывает хэндлеры в отношения Начало-Конец
+     * @param pairHandler хэндлер, с которым связываем
+     */
+    BindWithHandler(pairHandler: Handler) {
+        this._pairedHandler = pairHandler;
+        pairHandler._pairedHandler = this;
+    };
+
+
+    private _withTooltip: boolean;
 
     showTooltip() {
         this._withTooltip = true;
     }
+
     hideTooltip() {
         this._withTooltip = false;
+    }
+
+
+    private _bodyHTML: string;
+
+    constructor(withTooltip: boolean,) {
+
     }
 }
