347a25e188311e70cd082265d93da01e46a2d2b3
Atsuta Roman <indifferentrun@gmail.com>
2019-11-29 15:22:36 +0700

    Remove unneeded imports, declare constructor

--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,10 @@
 export default class Model {
-
+    constructor(parameters:
+                    {
+                        min?: number;
+                        max?: number;
+                        items?: Array<string>;
+                    }
+    ) {
+    }
 }
