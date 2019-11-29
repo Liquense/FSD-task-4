f6a9f74dd61409201a4764f12ac74dbd036f6d87
Atsuta Roman <indifferentrun@gmail.com>
2019-11-29 15:42:38 +0700

    add value argument and fix function description

--- src/liquidSlider.ts
+++ src/liquidSlider.ts
@@ -4,6 +4,13 @@ import Controller from "./controller"
 /**
  * Расширение JQuery, добавляющее функцию для инициализации слайдера
  * @param parameters
+ * @param parameters.additionalClass дополнительный класс
+ * @param parameters.items массив элементов, который будет перебираться слайдером
+ * (если не указан - будет массив чисел от min до max с шагом step)
+ * @param parameters.value текущее значение
+ * (если заданы кастомные значения, то текущее значение - номер элемента в массиве)
+ * @param parameters.isRange если хэндлеры не заданы вручную - определяет их количество (false - 1, true - 2)
+ * @param parameters.isVertical вертикальная ли ориентация
  * @param parameters.min минимальное значение слайдера
  * @param parameters.max максимальное значение слайдер
  * @param parameters.step шаг слайдера
@@ -16,15 +23,12 @@ import Controller from "./controller"
  *          @param parameters.handlers.tooltip.additionalClass дополнительный класс
  *          @param parameters.handlers.tooltip.position позиция относительно хэндлера
  *          @param parameters.handlers.tooltip.bodyHTML HTML-код, если нужно заменить стандартный
- * @param parameters.isVertical вертикальная ли ориентация
- * @param parameters.sliderClass дополнительный класс слайдера
- * @param parameters.items массив элементов, который будет перебираться слайдером
- * (если не указан - будет массив чисел от min до max с шагом step)
  */
 $.fn.liquidSlider = function liquidSlider(
     parameters?: {
         additionalClass?: string,
         items?: Array<any>,
+        value?: number,
         isRange?: boolean,
         isVertical?: boolean,
         min?: number,
