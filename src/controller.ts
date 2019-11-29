8e1dbe1ea41bbad80833ac1096f42bd2cad0c2fd
Atsuta Roman <indifferentrun@gmail.com>
2019-11-29 15:19:32 +0700

    now pass parameters to appropriate classes without destruct

--- src/controller.ts
+++ src/controller.ts
@@ -1,36 +1,15 @@
 import View from "./view"
 import Model from "./model"
-import {Handler} from "./slider/__handler/handler";
-import {Tooltip} from "./slider/__handler/__tooltip/tooltip";
 
 export default class Controller {
-    private _element: HTMLElement;
     private _view: View;
     private _model: Model;
 
     constructor(
-        DOMElement,
-        parameters: {
-            min: number;
-            isVertical: boolean;
-            max: number;
-            handlers: Handler[];
-            sliderClass: string;
-            tooltip: Tooltip;
-            items: Array<string>;
-            isRange: boolean
-        }) {
-        if (!Array.isArray(DOMElement)) {
-            throw new Error("There is no HTML elements founded");
-        }
-        if (DOMElement.length <= 0) {
-            throw new Error(`Array ${DOMElement} hasn't any elements`);
-        }
-        this._element = DOMElement[0];
-        this._model = new Model();
-        this._view = new View();
-
-        console.log(this._element);
+        DOMElement: Element,
+        parameters: object
+    ) {
+        this._model = new Model(parameters);
+        this._view = new View(DOMElement, parameters);
     }
-
 }
