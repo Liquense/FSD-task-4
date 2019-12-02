42726e534cebce00e549556d4ecd44e9e4e89e8e
Atsuta Roman <indifferentrun@gmail.com>
2019-12-02 19:03:47 +0700

    add second slider for tests

--- src/demoPage/index.ts
+++ src/demoPage/index.ts
@@ -3,3 +3,6 @@ import "../liquidSlider"
 import "../slider/slider.scss"
 
 let $slider = $(".initSliderHere").liquidSlider();
+let $slider2 = $(".initSliderHere").liquidSlider({
+    isRange: true,
+});
