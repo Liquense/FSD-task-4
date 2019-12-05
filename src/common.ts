bb8d49629d87626420afebfd893c6c40f15aadfc
Atsuta Roman <indifferentrun@gmail.com>
2019-12-05 15:57:30 +0700

    add functions for additional classes of DOM elements

--- src/common.ts
+++ src/common.ts
@@ -5,3 +5,18 @@ export const viewFunctionNames = [
 export const modelFunctionNames = [
     "modelTest",
 ];
+
+export function parseClassesString(classesString: string): string[] {
+    if (!classesString)
+        return undefined;
+    return classesString.split(" ");
+}
+
+export function addClasses(element: Element, classes: string[]) {
+    if (!classes)
+        return;
+
+    for (const className of classes) {
+        element.classList.add(className);
+    }
+}
