e74e0fc78e2c517e670c494046fe22ad697394cd
Atsuta Roman <indifferentrun@gmail.com>
2019-12-26 18:43:00 +0700

    ~add slider and some variety in init parameters

--- src/demoPage/index.ts
+++ src/demoPage/index.ts
@@ -2,7 +2,27 @@ import $ from "jquery";
 import "../liquidSlider"
 import "../slider/slider.scss"
 
-let $slider = $(".initSliderHere").liquidSlider();
+let $slider = $(".initSliderHere").liquidSlider({
+    min: -50,
+    max: 20,
+    step: 1
+});
+
 let $slider2 = $(".initSliderHere").liquidSlider({
-    isRange: true,
+    items: [
+        1,
+        {
+            toString() {
+                return "two"
+            }
+        },
+        "<div>three</div>"
+    ],
+    min: 0,
+    max: 22,
+    isRange: true
+});
+
+let $slider3 = $(".initSliderHere").liquidSlider({
+    handlers: [{value: 2}, {value: 6}, {value: 19}],
 });
