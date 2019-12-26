e671f9b5988261584878bb85ce8c3abcb1cedb0e
Atsuta Roman <indifferentrun@gmail.com>
2019-12-26 18:45:45 +0700

    +passing handler data between model and view

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,6 @@
 import View from "./view"
 import Model from "./model"
-import {addListener} from "./common";
+import {addListenerAfter} from "./common";
 
 export default class Controller {
     private _view: View;
@@ -13,20 +13,41 @@ export default class Controller {
         this._view = new View(DOMElement, parameters);
         this._model = new Model(parameters);
 
-        addListener("handlerValueChanged", this._view.handlersValuesChangedListener, this._model);
+        addListenerAfter("handlerValueChanged", this._view.handlersValuesChangedListener, this._model);
+        addListenerAfter("createHandler", this.viewAddHandler, this._model);
 
         const initialHandlersData = this._model.getHandlersData();
         this.passHandlersData(this._view, initialHandlersData);
+        const initialSliderData = this._model.getSliderData();
+        this.passSliderData(this._view, initialSliderData);
+
     }
 
-    private passHandlersData(whereToPass: Model | View,
-                             whatToPass:
-                                 { value: number, position: number, }[]
+
+    private passSliderData(
+        receiver: Model | View,
+        sliderData: {step: number},
     ) {
-        if (whereToPass instanceof Model) {
+        if (receiver instanceof Model) {
 
         } else {
-            this._view.setHandlersData(whatToPass);
+            this._view.setSliderData(sliderData);
         }
+    }
+
+    private passHandlersData(
+        receiver: Model | View,
+        handlersData: { value: any, position: number, }[]
+    ) {
+        if (receiver instanceof Model) {
+
+        } else {
+            this._view.setHandlersData(handlersData);
+        }
+    }
+
+    private viewAddHandler() {
+        console.log("binding!");
+        this._view.addHandler();
     }
 }
