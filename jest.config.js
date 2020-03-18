56682dce4f46975208270a1e7a770ec0800d52f2
Atsuta Roman <indifferentrun@gmail.com>
2020-03-19 01:14:45 +0700

    +define collecting coverage rules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	"collectCoverageFrom": [
+		"src/**/*.ts",
+		"!src/**/*.d.ts",
+		"!src/demoPage/**"
+	]
 };
