cb4cf7050d67215e936045f64a7156049d3a5ff0
Atsuta Roman <indifferentrun@gmail.com>
2020-03-23 15:45:33 +0700

    +cover liquidSlider with tests

---
+++ src/liquidSlider.test.ts
@@ -0,0 +1,16 @@
+jest.mock("./controller");
+import * as $ from 'jquery';
+import "./liquidSlider"
+import Controller from "./controller";
+
+let $testDiv = $(document.body.appendChild(document.createElement("div")));
+
+test("Инициализация слайдера", () => {
+    let testParameters = {something: "", anotherArg: "test1", withMarkup: ""};
+
+    $testDiv.liquidSlider();
+    expect(Controller).toBeCalledWith($testDiv.get()[0], undefined);
+
+    $testDiv.liquidSlider(testParameters);
+    expect(Controller).toBeCalledWith($testDiv.get()[0], testParameters);
+});
